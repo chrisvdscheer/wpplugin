1d372e76dbb9f19d1297c5ecba2a67b6eb54a005
Chris van der Scheer <chrisvanderscheer@macbookvanchris.lan>
2018-07-27 16:19:57 +0200

    Initial Commit

---
+++ wpplugin/wpplugin.php
@@ -0,0 +1,14 @@
+<?php 
+
+/*
+Plugin Name:  WordPress.org Plugin
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  Basic WordPress Plugin Header Comment
+Version:      20160911
+Author:       WordPress.org
+Author URI:   https://developer.wordpress.org/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+*/	
